69e66cf95351c5f8b6714d173f34744270816ab1
吴冬 <346607313@qq.com>
2018-03-19 14:20:13 +0800

    wdzooGo

---
+++ WD_ZOO/WD_ZOO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WD_ZOO
+//
+//  Created by 吴冬 on 2018/3/19.
+//  Copyright © 2018年 吴冬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WD_ZOO/WD_ZOO/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  WD_ZOO
+//
+//  Created by 吴冬 on 2018/3/19.
+//  Copyright © 2018年 吴冬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
